0c5c1e2c966182f6d511c2c1d259aa49f820d04d
rebeccaloaustin <rebeccaloaustin@gmail.com>
2024-02-24 10:30:42 -0500

    set up firebase connection

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Hero from "../components/Hero";
+import Hero from "../components/MovieBanner";
 import MovieRow from "../components/MovieRow";
 import endpoints from "../services/movieServices";
 
